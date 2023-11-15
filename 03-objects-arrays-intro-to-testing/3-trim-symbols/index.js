e1811ad46510558895fbd11a9b1be18f9bfb7fd3
Maria <maria.badanina.s@gmail.com>
2023-11-15 16:40:56 -0500

    3rd exercise of 3rd task complete

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,27 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (typeof size === "undefined") {
+    return string;
+  }
+  if (size === 0) {
+    return "";
+  }
+  let result = "";
+  let count = 0;
+  let prevChar = "";
+  for (const char of string) {
+    if (char !== prevChar) {
+      count = 1;
+      prevChar = char;
+      result += char;
+    } else {
+      if (count < size) {
+        result += char;
+        count++;
+      }
+    }
+  }
 
+  return result;
 }
