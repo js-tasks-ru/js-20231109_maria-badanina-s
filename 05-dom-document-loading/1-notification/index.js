89ee5848946dd13ea77b7c79a56dcf633f378267
Maria <maria.badanina.s@gmail.com>
2023-11-28 14:54:07 -0500

    1st exercise of 5ft task complete

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,45 @@
 export default class NotificationMessage {
+  constructor(message = "Default message", options = {}) {
+    const { type = "success", duration = 0 } = options;
 
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.element = document.createElement("div");
+    this.element.classList.add("notification", this.type);
+    this.element.style.setProperty("--value", `${this.duration / 1000}s`);
+    this.element.innerHTML = this.createTemplate();
+    document.body.appendChild(this.element);
+  }
+
+  createTemplate() {
+    return `
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+           ${this.message}
+          </div>
+        </div>
+    `;
+  }
+
+  show(notificationDiv) {
+    if (notificationDiv instanceof HTMLDivElement) {
+      this.element = notificationDiv;
+    }
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
