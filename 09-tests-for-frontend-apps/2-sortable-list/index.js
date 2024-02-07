5adbb0ed2bf35078c1d07ac85a125286eed88428
Maria <maria.badanina.s@gmail.com>
2024-02-06 21:03:43 -0500

    1st exercise of 9th task WIP

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -37,111 +37,112 @@ export default class SortableList {
     item.remove();
   }
 
-  onElementDrag(event) {
+  onElementDrag = (event) => {
     const item = event.target.closest(".sortable-list__item");
     const dragButton = event.target.closest("[data-grab-handle]");
 
-    if (event.target != dragButton) return;
+    if (event.target !== dragButton) return;
 
-    let shiftX = event.clientX - item.getBoundingClientRect().left;
-    let shiftY = event.clientY - item.getBoundingClientRect().top;
+    const shiftX = event.clientX - item.getBoundingClientRect().left;
+    const shiftY = event.clientY - item.getBoundingClientRect().top;
 
-    // put placeholder in the place of the grabbed element
+    const placeholder = this.createPlaceholder(item);
+    this.insertPlaceholder(item, placeholder);
+
+    this.setupDragStyles(item);
+
+    const onMouseMove = (event) => {
+      this.moveItem(item, shiftX, shiftY, event);
+      this.handleDroppable(item, placeholder, event);
+    };
+
+    document.addEventListener("pointermove", onMouseMove);
+
+    item.addEventListener("mouseup", () =>
+      this.handleMouseUp(item, placeholder, onMouseMove)
+    );
+  };
+
+  createPlaceholder() {
     const placeholder = document.createElement("li");
     placeholder.classList.add(
       "sortable-list__placeholder",
       "sortable-list__item"
     );
-    // Get the index of the original element within its parent
-    const index = Array.from(this.children).indexOf(item);
-    // Insert the placeholder at the same index as the original element
-    this.insertBefore(placeholder, this.children[index + 1]);
+    return placeholder;
+  }
 
+  insertPlaceholder(item, placeholder) {
+    const index = Array.from(this.element.children).indexOf(item);
+    this.element.insertBefore(placeholder, this.element.children[index + 1]);
+  }
+
+  setupDragStyles(item) {
     item.style.position = "fixed";
     item.style.zIndex = 1000;
     item.style.width = "100%";
+  }
 
-    function moveAt(pageX, pageY) {
-      item.style.left = pageX - shiftX + "px";
-      item.style.top = pageY - shiftY + "px";
-    }
+  moveItem(item, shiftX, shiftY, event) {
+    item.style.left = event.pageX - shiftX + "px";
+    item.style.top = event.pageY - shiftY + "px";
+  }
 
-    const onMouseMove = (event) => {
-      moveAt(event.pageX, event.pageY);
+  handleDroppable(item, placeholder, event) {
+    item.style.visibility = "hidden";
+    const elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+    item.style.visibility = "visible";
 
-      item.style.visibility = "hidden";
-      let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+    const droppableBelow = elemBelow?.closest(".sortable-list__item");
 
-      if (!elemBelow) return;
-
-      let droppableBelow = elemBelow.closest(".sortable-list__item");
-
-      item.style.visibility = "visible";
-
-      if (this.currentDroppable != droppableBelow) {
-        this.currentDroppable = droppableBelow;
-        if (this.currentDroppable) {
-          enterDroppable(this.currentDroppable, placeholder);
-        }
-      }
-    };
-
-    document.addEventListener("pointermove", onMouseMove);
-
-    item.addEventListener("mouseup", onMouseUp);
-
-    function onMouseUp() {
-      document.removeEventListener("pointermove", onMouseMove);
-      item.removeEventListener("dragstart", dragStartHandler);
-      leaveDroppable(item, placeholder);
-
-      // Remove the event listener to avoid potential memory leaks
-      item.removeEventListener("mouseup", onMouseUp);
-    }
-
-    item.addEventListener("dragstart", dragStartHandler);
-    function dragStartHandler(event) {
-      event.preventDefault();
-    }
-
-    function enterDroppable(element1, element2) {
-      /// here I need to swap elem with placeholder
-      if (element1?.parentNode && element2?.parentNode) {
-        // Create a temporary storage for element1
-        let temp = document.createElement("div");
-
-        // Step in between to hold the place of element1
-        element1.parentNode.insertBefore(temp, element1);
-
-        // Replace element1 with element2
-        element2.parentNode.replaceChild(element1, element2);
-
-        // Move element2 to the original position of element1
-        temp.parentNode.replaceChild(element2, temp);
+    if (this.currentDroppable !== droppableBelow) {
+      this.currentDroppable = droppableBelow;
+      if (this.currentDroppable) {
+        this.enterDroppable(this.currentDroppable, placeholder);
       }
     }
+  }
 
-    function leaveDroppable(item, placeholder) {
-      /// here I need to swap elem with placeholder
-      if (item?.parentNode && placeholder?.parentNode) {
-        // Create a temporary storage for item
-        let temp = document.createElement("div");
-
-        // Step in between to hold the place of item
-        item.parentNode.insertBefore(temp, item);
-
-        // Replace item with placeholder
-        placeholder.parentNode.replaceChild(item, placeholder);
-
-        // Move placeholder to the original position of item
-        temp.parentNode.replaceChild(placeholder, temp);
-        placeholder.remove();
-
-        item.style.position = "static";
-        item.style.zIndex = 1;
-        item.style.width = "auto";
-      }
+  enterDroppable(element1, element2) {
+    if (element1?.parentNode && element2?.parentNode) {
+      this.swapElements(element1, element2);
     }
+  }
+
+  swapElements(element1, element2) {
+    const temp = document.createElement("div");
+
+    element1.parentNode.insertBefore(temp, element1);
+    element2.parentNode.replaceChild(element1, element2);
+    temp.parentNode.replaceChild(element2, temp);
+  }
+
+  handleMouseUp(item, placeholder, onMouseMove) {
+    document.removeEventListener("pointermove", onMouseMove);
+    item.removeEventListener("dragstart", this.dragStartHandler);
+    this.leaveDroppable(item, placeholder);
+    item.removeEventListener("mouseup", () =>
+      this.handleMouseUp(item, placeholder, onMouseMove)
+    );
+  }
+
+  leaveDroppable(item, placeholder) {
+    if (item?.parentNode && placeholder?.parentNode) {
+      const temp = document.createElement("div");
+
+      item.parentNode.insertBefore(temp, item);
+      placeholder.parentNode.replaceChild(item, placeholder);
+      temp.parentNode.replaceChild(placeholder, temp);
+      placeholder.remove();
+
+      this.resetDragStyles(item);
+    }
+  }
+
+  resetDragStyles(item) {
+    item.style.position = "static";
+    item.style.zIndex = 1;
+    item.style.width = "auto";
   }
 
   destroy = () => {
